4aaf8827edf221b36cc81b143985ace22fe384a1
Jiyoon Jung <jiyoonique@gmail.com>
2020-02-21 14:04:26 -0800

    Implment api to handle multiple tags

--- server/controller.js
+++ server/controller.js
@@ -32,21 +32,67 @@ const controller = {
       res.status(400).send({error: 'directionFields parameter is invalid'})
     }
 
-    axios.get(`https://hatchways.io/api/assessment/blog/posts`, {
-      params: {
-        tag: tags,
-        sortBy: sortBy,
-        direction: direction
-      }
-    })
-    .then(results => {
-      res.status(200).send(results.data);
-    })
-    .catch(err => {
-      res.status(400).send(`Unsuccessful getAllData request due to following error: ${err}`);
-    })
+    if(tags.indexOf(',') !== -1){
+
+      const tagsCollection = tags.split(',');
+      const allResults = tagsCollection.map((tag) => {
+        //return axios.get(`https://hatchways.io/api/assessment/blog/posts?tag=${tag}&sortBy=${sortBy}&direction=${direction}`)
+        return axios.get(`https://hatchways.io/api/assessment/blog/posts`, {
+          params: {
+            tag: tag,
+            sortBy: sortBy,
+            direction: direction
+          }
+        })
+      })
+
+      axios.all(allResults)
+      .then(axios.spread((...responses) => {
+        let dataCollection = {};
+        responses.forEach(response => {
+          for (var i = 0; i < response.data.posts.length; i++){
+            let singleData = response.data.posts[i]
+            dataCollection[singleData.id] = singleData;
+          }
+        })
+        let allCollection = Object.values(dataCollection);
+        if (sortBy){
+          if (direction === 'desc'){
+            allCollection = allCollection.sort((a, b) => (
+              b[sortBy] - a[sortBy]
+            ))
+          } else {
+            allCollection = allCollection.sort((a, b) => (
+              a[sortBy] - b[sortBy]
+            ))
+          }
+        }
+        res.status(200).send(allCollection)
+      }))
+      .catch(err => {
+        res.status(400).send({
+          error: 'Tags parameter is required'
+        })
+      });
+
+    } else {
+
+      axios.get(`https://hatchways.io/api/assessment/blog/posts`, {
+        params: {
+          tag: tags,
+          sortBy: sortBy,
+          direction: direction
+        }
+      })
+      .then(results => {
+        res.status(200).send(results.data);
+      })
+      .catch(err => {
+        res.status(400).send(`Unsuccessful getAllData request due to following error: ${err}`);
+      })
+
+    }
   }
- 
 }
 
 module.exports = controller;
